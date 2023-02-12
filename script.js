5620d015874256f525605c205b9bd053b78850ce
GoneHollow <44032968+GoneHollow@users.noreply.github.com>
2023-02-12 15:32:09 +0100

    basic functionality and style
    - setRandomImages() is now async
    - css overhaul
    - functionality of memory game

--- script.js
+++ script.js
@@ -3,8 +3,17 @@
 const catPics = [];
 const board = document.getElementById("board");
 const startButton = document.getElementById("startButton");
+const headline = document.getElementById("headline");
+const div = document.getElementsByTagName("div");
+const solvedElems = document.getElementsByClassName("solved");
 
-async function getImage() 
+let score = document.getElementById("score");
+const elem = document.getElementsByClassName("element");
+console.log(catPics);
+
+startButton.addEventListener("click", setRandomImages);
+
+async function getImages() 
 {
     let response = await fetch(
       "https://api.thecatapi.com/v1/images/search?limit=8&hasBreeds=1",
@@ -16,6 +25,7 @@ async function getImage()
     );
 
     let img = await response.json();
+
     for (i = 0; i < 8; i++)
     {
         catPics.push(img[i]);
@@ -29,18 +39,27 @@ async function getImage()
     }
 }
 
-getImage();
-console.log(catPics);
 
-function setRandomImages() 
+
+async function setRandomImages() 
 {
+    await getImages();
+    startButton.style.transform = "translateX(-650px) translateY(10px)";
+    startButton.style.display = "inline-block";
+    headline.style.display = "none";
+    board.style.display = "";
+    startButton.value = "Restart";
+    
     shuffle(catPics);
+
     const fragment = document.createDocumentFragment();
     for (i = 0; i < 16; i++)
     {
         const div = document.createElement("div");
-        div.id = "column";
+        div.className = "element";
+        div.addEventListener("click", flipCards);
         const img = document.createElement("img");
+        div.id = catPics[i].id;
         fragment.appendChild(div);
         img.src = catPics[i].url;
         div.appendChild(img);
@@ -48,9 +67,6 @@ function setRandomImages()
     board.innerHTML = "";
     board.appendChild(fragment);
 }
-
-
-  startButton.addEventListener("click", setRandomImages)
 
 function shuffle(a) 
 {
@@ -63,4 +79,47 @@ function shuffle(a)
         a[j] = x;
     }
     return a;
+}
+
+function flipCards(event)
+{
+  const activeElements = document.getElementsByClassName("active");
+
+  if(activeElements.length < 2)
+  {
+    event.target.classList.add("active");
+  }
+
+  console.log(activeElements.length);
+
+  if(activeElements.length == 2)
+  {
+    if(activeElements[0].id == activeElements[1].id)
+    {
+      activeElements[0].classList.add("solved");
+      activeElements[1].classList.add("solved");
+
+      activeElements[0].removeEventListener("click", flipCards);
+      activeElements[1].removeEventListener("click", flipCards);
+
+
+      activeElements[1].classList.remove("active");
+      activeElements[0].classList.remove("active");
+
+      score.innerText = parseInt(score.innerText) + 100;
+
+      console.log(solvedElems);
+    } else
+    {
+      setTimeout(function(){
+        activeElements[1].classList.remove("active");
+        activeElements[0].classList.remove("active");
+      },2000)
+
+
+
+      
+    }
+  }
+
 }
